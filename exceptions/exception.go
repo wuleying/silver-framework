a05e1404737b9b9048834d43cf2c9fa313cc7ca9
wuleying <5590548@qq.com>
2018-03-27 17:19:43 +0800

    Delete test codes.

--- exceptions/exception.go
+++ exceptions/exception.go
@@ -3,7 +3,6 @@ package exceptions
 import (
 	"github.com/go-clog/clog"
 	"github.com/wuleying/silver-framework/globals"
-	"log"
 	"runtime"
 )
 
@@ -11,9 +10,6 @@ import (
 func CheckError(err error) {
 	if err != nil {
 		_, file, line, _ := runtime.Caller(1)
-		log.Println(file)
-		log.Println(line)
-
 		clog.Fatal(globals.ClogSkipDefault, "%s:%d %s", file, line, err.Error())
 	}
 }
