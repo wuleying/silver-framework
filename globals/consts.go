02ce417b45f9dc036ea08700bb1141f5b29ded7b
wuleying <5590548@qq.com>
2018-03-28 10:47:26 +0800

    Add consts

--- globals/consts.go
+++ globals/consts.go
@@ -5,18 +5,33 @@ const (
 	ProjectName = "silver-framework"
 	Version     = "0.0.1"
 
-	// 权限
+	// 配置文件路径
+	ConfigFilePath        = "config.ini"
+	ConfigDefaultFilePath = "config.default.ini"
+
+	// Clog skip 级别
+	ClogSkipDefault     = 0
+	ClogSkipDisplayInfo = 2
+)
+
+// 权限
+const (
 	DBFileModel   = 0600
 	FileReadMode  = 0644
 	FileWriteMode = 0666
 	DirReadMode   = 0755
 	DirWriteMode  = 0777
+)
 
-	// Clog skip 级别
-	ClogSkipDefault     = 0
-	ClogSkipDisplayInfo = 2
-
-	// 配置文件路径
-	ConfigFilePath        = "config.ini"
-	ConfigDefaultFilePath = "config.default.ini"
+// 存储大小
+const (
+	_   = 1 << (10 * iota)
+	KiB // 1024
+	MiB // 1048576
+	GiB // 1073741824
+	TiB // 1099511627776
+	PiB // 1125899906842624
+	EiB // 1152921504606846976
+	ZiB // 1180591620717411303424
+	YiB // 1208925819614629174706176
 )
