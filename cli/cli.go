7150faeaa553c42914428e598c4ede303f04b68e
wuleying <5590548@qq.com>
2018-04-12 14:56:50 +0800

    Update

--- cli/cli.go
+++ cli/cli.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	ucli "github.com/urfave/cli"
-	"github.com/wuleying/silver-framework/cli/commands"
 	"github.com/wuleying/silver-framework/globals"
 	"os"
 )
@@ -13,6 +12,18 @@ func main() {
 
 // Run 运行命令行
 func run() {
+	var commandList = []ucli.Command{
+		{
+			Name:    "version",
+			Aliases: []string{"V"},
+			Usage:   "Print version info",
+			Action: func(c *ucli.Context) error {
+				println(globals.Version)
+				return nil
+			},
+		},
+	}
+
 	app := ucli.NewApp()
 	app.Name = "Silver Framework"
 	app.Usage = "CLI tools"
@@ -23,6 +34,6 @@ func run() {
 			Email: "lolooo@live.com",
 		},
 	}
-	app.Commands = commands.Commands
+	app.Commands = commandList
 	app.Run(os.Args)
 }
