ee9db0f60366d3633b2960a22c53e9cd878a59cb
wuleying <5590548@qq.com>
2018-08-17 20:00:42 +0800

    Modify log method.

--- main.go
+++ main.go
@@ -1,44 +1,17 @@
 package main
 
 import (
-	"fmt"
 	"github.com/go-clog/clog"
 	"github.com/wuleying/silver-framework/admin"
 	"github.com/wuleying/silver-framework/config"
 	"github.com/wuleying/silver-framework/exceptions"
+	"github.com/wuleying/silver-framework/llog"
 	"github.com/wuleying/silver-framework/metrics"
-	"os"
 )
 
-// initClog
-func initClog() {
-	if err := clog.New(clog.CONSOLE, clog.ConsoleConfig{
-		Level:      clog.INFO,
-		BufferSize: 100,
-	}); err != nil {
-		fmt.Printf("[INFO] Init console log failed. error %+v.", err)
-		os.Exit(1)
-	}
-
-	if err := clog.New(clog.FILE, clog.FileConfig{
-		Level:      clog.INFO,
-		BufferSize: 100,
-		Filename:   "logs/clog.log",
-		FileRotationConfig: clog.FileRotationConfig{
-			Rotate: true,
-			Daily:  true,
-		},
-	}); err != nil {
-		fmt.Printf("[INFO] Init console log failed. error %+v.", err)
-		os.Exit(1)
-	}
-}
-
-// initRequestID
-
 func main() {
-	// Clog
-	initClog()
+	// Log init
+	llog.Init()
 	defer clog.Shutdown()
 
 	cfg, err := config.Init()
